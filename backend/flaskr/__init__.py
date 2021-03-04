fd966f96029b138dafd3335ef9506648a411bdd9
Moritz Jansen <mojansen95@gmail.com>
2021-03-04 14:41:33 +0100

    added docker files

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,5 +13,7 @@ sia = SentimentIntensityAnalyzer()
 def analyze_sentiment():
     input_text = request.get_json()["text"]
     sentiment = sia.polarity_scores(input_text)["compound"]
-    
     return {"sentiment": sentiment}
+
+if __name__ == "__main__":
+    app.run(port=80, host='0.0.0.0')
